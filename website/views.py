f9f17fe61945324a473d35da18b07129a35197cc
Teja2106 <teja182106@gmail.com>
2024-01-06 21:28:39 +0530

    Record Adding

--- website/views.py
+++ website/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from .forms import SignUpForm
+from .forms import SignUpForm, AddRecordForm
 from .models import Record
 
 # Create your views here.
@@ -61,4 +61,27 @@ def customer_record(request, pk):
         return render(request, 'record.html', {'customer_record': customer_record})
     else:
         messages.success(request, "You must be logged in to view that page.")
+        return redirect('home')
+    
+def delete_record(request, pk):
+    if request.user.is_authenticated:
+        delete_it = Record.objects.get(id=pk)
+        delete_it.delete()
+        messages.success(request, "Record deleted successfully...")
+        return redirect('home')
+    else:
+        messages.success(request, "You must be logged in to do that...")
+        return redirect('home')
+    
+def add_record(request):
+    form = AddRecordForm(request.POST or None)
+    if request.user.is_authenticated:
+        if request.method == "POST":
+            if form.is_valid():
+                add_record = form.save()
+                messages.success(request, "Record Added")
+                return redirect('home')
+        return render(request, 'add_record.html', {'form': form})
+    else:
+        messages.success(request, 'You must be logged in...')
         return redirect('home')
